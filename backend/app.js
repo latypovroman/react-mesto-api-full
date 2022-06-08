32ce20911897f0a73a15fec376040fcdc575335f
latypovroman <jiggawhat5@gmail.com>
2022-06-09 00:22:45 +0500

    after 1st review

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(requestLogger);
 app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:3000/', 'https://mesto.app.nomoredomains.sbs'],
-  credentials: true,
+  // credentials: true,
 }));
 
 app.get('/crash-test', () => {
